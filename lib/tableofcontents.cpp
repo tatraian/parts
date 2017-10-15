d0a15a38e707e8b4d74f6971c66d2c87250dbb2b
Tatrai Antal <tatraiantal@gmail.com>
2017-10-15 22:35:59 +0200

    TableOfContents is in progress

--- lib/tableofcontents.cpp
+++ lib/tableofcontents.cpp
@@ -1,1 +1,25 @@
 #include "tableofcontents.h"
+
+#include <boost/filesystem.hpp>
+
+using namespace parts;
+
+const std::string DEFAULT_OWNER = "__PARTS_DEFAULT_OWNER__";
+const std::string DEFAULT_GROUP = "__PARTS_DEFAULT_GROUP__";
+
+
+//==========================================================================================================================================
+TableOfContents::TableOfContents(const boost::filesystem::path& root, const PartsCompressionParameters& parameters) :
+    m_parameters(parameters)
+{
+    m_owners.push_back(DEFAULT_OWNER);
+    m_groups.push_back(DEFAULT_GROUP);
+
+    if (!boost::filesystem::exists(root))
+        throw PartsException("Root directory doesn't exist: " + root.string());
+
+    if (boost::filesystem::is_regular_file(root))
+    {
+
+    }
+}
--- lib/tableofcontents.h
+++ lib/tableofcontents.h
@@ -2,10 +2,16 @@
 #define TABLEOFCONTENTS_H
 
 #include "baseentry.h"
+#include "parts_definitions.h"
 
 #include <map>
 #include <memory>
+#include <vector>
+#include <string>
+
 #include <boost/filesystem/path.hpp>
+
+#include <sys/stat.h>
 
 namespace parts
 {
@@ -13,12 +19,21 @@ namespace parts
 class TableOfContents
 {
 public:
-    TableOfContents(const boost::filesystem::path& root);
+    TableOfContents(const boost::filesystem::path& root, const PartsCompressionParameters& parameters);
 
     std::vector<uint8_t> getRaw() const;
 
 protected:
+    uint16_t getOwnerId(const stat* file_stat);
+    uint16_t getGroupId(const stat* file_stat);
+    uint16_t getPermissions(const stat* file_stat);
+
+protected:
     std::map<boost::filesystem::path, std::shared_ptr<BaseEntry> > m_files;
+    std::vector<std::string> m_owners;
+    std::vector<std::string> m_groups;
+
+    const PartsCompressionParameters& m_parameters;
 };
 
 }
